2423e9077521d26734a187968c6b2777c022075a
nathan-g1 <nathygech111@gmail.com>
2024-10-20 22:34:44 -0500

    update

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gcc -o pp -std=c99 -pthread pp.c -pg
-gcc -o ss -std=c99 -pthread ss.c -pg
+gcc -o pp -std=c99 -pthread qsort_parallel.c -pg
+gcc -o ss -std=c99 -pthread qsort_sec.c -pg
 
 ./pp
 ./ss
